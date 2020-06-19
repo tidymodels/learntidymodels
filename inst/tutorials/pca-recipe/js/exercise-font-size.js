4a1a8221730ce042fc0d6613e6ff75597ef1c2a5
Ezgi RStudio <ezgi.karaesmen@gmail.com>
2020-06-18 23:19:36 -0400

    change code font size to 14, add fill in the blank exercises, sortable exercise for UMAP and section for UMAP hyperparameters

--- inst/tutorials/pca-recipe/js/exercise-font-size.js
+++ inst/tutorials/pca-recipe/js/exercise-font-size.js
@@ -2,6 +2,6 @@ $(function() {
    var editor;
    $('.ace_editor').each(function( index ) {
      editor = ace.edit(this);
-     editor.setFontSize("16px");
+     editor.setFontSize("14px");
    });
 })

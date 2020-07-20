3577950734314908d8c42cf160ac809ddd026094
Ezgi RStudio <ezgi.karaesmen@gmail.com>
2020-07-20 15:17:47 -0400

    remove palmerpenguins from depend, add spotify dataset to package

---
+++ data-raw/spotify_raw.R
@@ -0,0 +1,19 @@
+## code to prepare `DATASET` dataset goes here
+
+library(tidyverse)
+
+set.seed(1234)
+spotify <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-01-21/spotify_songs.csv') %>%
+    mutate(
+        track_album_release_year =
+            track_album_release_date %>%
+            str_split("-", simplify = TRUE) %>%
+            .[,1] %>%
+            as.integer()
+        ,playlist_genre = as.factor(playlist_genre)
+    ) %>%
+    select(-track_album_release_date) %>%
+    slice_sample(prop = 0.2, replace=FALSE)
+
+
+usethis::use_data(spotify, overwrite = TRUE)

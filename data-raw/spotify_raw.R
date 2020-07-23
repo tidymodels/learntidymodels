d25854df772520cf4dfd4881a72f85731cb00ba4
Ezgi RStudio <ezgi.karaesmen@gmail.com>
2020-07-23 12:08:58 -0400

    fix bad column name

--- data-raw/spotify_raw.R
+++ data-raw/spotify_raw.R
@@ -9,8 +9,10 @@ spotify <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/ti
             track_album_release_date %>%
             str_split("-", simplify = TRUE) %>%
             .[,1] %>%
-            as.integer()
-        ,playlist_genre = as.factor(playlist_genre)
+            as.integer(),
+        playlist_genre = recode(playlist_genre, `r&b`="rnb"),
+        playlist_genre = as.factor(playlist_genre)
+
     ) %>%
     select(-track_album_release_date) %>%
     slice_sample(prop = 0.2, replace=FALSE)
